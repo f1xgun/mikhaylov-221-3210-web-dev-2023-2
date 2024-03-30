eba079e00add34954c9fa5f0f40c792e50f7da52
f1xgun <msnzlmm@mail.ru>
2024-03-30 10:13:53 +0300

    fix lab3, add formatted number in result

--- lab2/app/app.py
+++ lab2/app/app.py
@@ -4,24 +4,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 application = app
 
-requests_data = []
+requests_data = []    
 
-def check_number(number: str) -> str:
+def check_number(number: str) -> {str, str}:
     number = re.sub(r'[-\(\)\s.]', '', number)
 
     digits_count = sum(map(lambda x: x.isdigit(), number))
     number = number.replace("+", "") # В задании указано, что убираем "+" как доп символ, поэтому убираем все, а не только в начале
     if not(digits_count == 10 or digits_count == 11 and (number[0] == "7" or number[0] == "8")):
-        return "Недопустимый ввод. Неверное количество цифр."
+        return "Недопустимый ввод. Неверное количество цифр.", ""
    
     if digits_count != len(number):
-        return "Недопустмый ввод. В номере телефона встречаются недопустимые символы"
+        return "Недопустмый ввод. В номере телефона встречаются недопустимые символы", ""
     
     number_without_prefix: str = number[len(number) - 10:]
     formatted_number = f"8-{number_without_prefix[:3]}-{number_without_prefix[3:6]}-{number_without_prefix[6:8]}-{number_without_prefix[8:]}"
 
-    print(formatted_number)
-    return ""
+    return "", formatted_number
 
 @app.route('/')
 def index():
@@ -29,11 +28,12 @@ def index():
 
 @app.route('/form', methods=["GET", "POST"])
 def form():
-    error = ""
-    if request.method == "POST":
-        error = check_number(request.form.get("phone_number"))
     log_request()
-    return render_template('form.html', error=error)
+    if request.method == "GET":
+        return render_template('form.html')
+
+    error, number = check_number(request.form.get("phone_number", ""))
+    return render_template('form.html', error=error, number=number)
 
 @app.route('/requests')
 def requests():
